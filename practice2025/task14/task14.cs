d41734425eeea4e4fb5f5e811aea61057dc0aa80
Elena <ulidka12345@yandex.ru>
2025-07-16 01:48:26 +0600

    feat: add task15

--- practice2025/task14/task14.cs
+++ practice2025/task14/task14.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿/*
+using System.Threading;
 
 namespace task14
 {
@@ -38,8 +39,76 @@ namespace task14
             barrier.SignalAndWait();
             barrier.Dispose();
 
+
+
             return total_result;
+        }
+        public static double SolveSingleThread(double a, double b, Func<double, double> function, double step)
+        {
+            double result = 0.0;
+
+            for (double x = a; x < b; x += step)
+            {
+                double next = Math.Min(x + step, b);
+                result += (function(x) + function(next)) / 2.0 * (next - x);
+            }
+
+            return result;
+        }
+    }
+}*/
+using System.Threading;
+
+namespace task14
+{
+    public static class DefiniteIntegral
+    {
+        public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
+        {
+            if (threadsnumber <= 1)
+            {
+                return SolveSingleThread(a, b, function, step);
+            }
+
+            double length = (b - a) / threadsnumber;
+            int n = (int)(length / step);
+            int chunkSize = n / threadsnumber;
+            double result = 0;
+            object locker = new object();
+
+            Parallel.For(0, threadsnumber, i =>
+            {
+                double thread_start = a + i * length;
+                double thread_end = (i == threadsnumber - 1) ? b : thread_start + length;
+
+                double total_sum = 0.0;
+                for (double a = thread_start; a < thread_end; a += step)
+                {
+                    double b = Math.Min(a + step, thread_end);
+                    total_sum += (function(a) + function(b)) / 2.0 * (b - a);
+                }
+
+                lock (locker)
+                {
+                    result += total_sum;
+                }
+            });
+
+            return result;
+        }
+        public static double SolveSingleThread(double a, double b, Func<double, double> function, double step)
+        {
+            double sum_res = 0;
+            int n = (int)((b - a) / step);
+            for (int i = 0; i < n; i++)
+            {
+                double start = a + i * step;
+                double end = start + step;
+                double sum = (function(start) + function(end)) * step / 2.0;
+                sum_res += sum;
+            }
+
+            return sum_res;
         }
     }
 }
-
