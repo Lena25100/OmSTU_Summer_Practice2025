4951f4a35c8817789e4723d457291205dd2264b5
Elena <ulidka12345@yandex.ru>
2025-07-16 02:04:22 +0600

    feat: add task15

--- practice2025/task14/task14.cs
+++ practice2025/task14/task14.cs
@@ -2,44 +2,52 @@
 
 namespace task14
 {
-    public class DefiniteIntegral
+    public static class DefiniteIntegral
     {
         public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
         {
-            double total_result = 0.0;
+            if (threadsnumber <= 1)
+            {
+                return SolveSingleThread(a, b, function, step);
+            }
 
             double length = (b - a) / threadsnumber;
+            double result = 0;
+            object locker = new object();
 
-            Barrier barrier = new Barrier(threadsnumber + 1);
-
-            for (int i = 0; i < threadsnumber; i++)
+            Parallel.For(0, threadsnumber, i =>
             {
                 double thread_start = a + i * length;
                 double thread_end = (i == threadsnumber - 1) ? b : thread_start + length;
 
-                Thread thread = new Thread(_ =>
+                double total_sum = 0.0;
+                for (double a = thread_start; a < thread_end; a += step)
                 {
+                    double b = Math.Min(a + step, thread_end);
+                    total_sum += (function(a) + function(b)) / 2.0 * (b - a);
+                }
 
-                    double total_sum = 0.0;
-                    for (double a = thread_start; a < thread_end; a += step)
-                    {
-                        double b = Math.Min(a + step, thread_end);
-                        total_sum += (function(a) + function(b)) / 2.0 * (b - a);
-                    }
+                lock (locker)
+                {
+                    result += total_sum;
+                }
+            });
 
-                    Interlocked.Exchange(ref total_result, total_result + total_sum);
-
-                    barrier.SignalAndWait();
-                });
-
-                thread.Start();
+            return result;
+        }
+        public static double SolveSingleThread(double a, double b, Func<double, double> function, double step)
+        {
+            double sum_res = 0;
+            int n = (int)((b - a) / step);
+            for (int i = 0; i < n; i++)
+            {
+                double start = a + i * step;
+                double end = start + step;
+                double sum = (function(start) + function(end)) * step / 2.0;
+                sum_res += sum;
             }
 
-            barrier.SignalAndWait();
-            barrier.Dispose();
-
-            return total_result;
+            return sum_res;
         }
     }
 }
-
--- practice2025/task14tests/task14tests.cs
+++ practice2025/task14tests/task14tests.cs
@@ -25,4 +25,3 @@ namespace task14tests
         }
     }
 }
-
